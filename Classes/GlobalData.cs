0c8fba000763b3cd2a051c0583f2bd86644ee05d
AzureAD\NimeshEkanayakePlatn <nimesh.e@platned.com>
2024-11-04 11:45:13 +0530

    +/- migrated from platned cloud custom pages to PlatnedPass component in Demo environment

---
+++ Classes/GlobalData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace PlatnedMahara.Classes
+{
+    public static class GlobalData
+    {
+        // Internal PlatnedPass access details
+        // Do not change unless instance changes -- START
+        /*public static readonly string BaseUrlPl = "https://cloud.platnedcloud.com";
+        public static readonly string AccessTokenUrlPl = "https://cloud.platnedcloud.com/auth/realms/platprd/protocol/openid-connect/token";
+        public static readonly string ClientIdPl = "Plat_APT_Service";
+        public static readonly string ClientSecretPl = "JawpIl0UXtCABshpP8TlWHFL9ghtzGue";
+        public static readonly string ScopePl = "openid microprofile-jwt";*/
+
+        public static readonly string BaseUrlPl = "https://ifscloud-demo.platnedcloud.com";
+        public static readonly string AccessTokenUrlPl = $"{BaseUrlPl}/auth/realms/platdmo/protocol/openid-connect/token";
+        public static readonly string ClientIdPl = "PlatnedPass_Service";
+        public static readonly string ClientSecretPl = "XSgJKPsBNb0C3Rhv4d0nF84ag4rBZcwC";
+        public static readonly string ScopePl = "openid microprofile-jwt";
+
+        // Do not change unless instance changes -- END
+
+        //for later
+        public static string? UserId { get; set; }
+        public static string? UserName { get; set; }
+        public static string? CompanyId { get; set; }
+    }
+}
